98c5b399c4b75feaaf65661b05e4271a5bd441fc
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-06-03 22:00:57 -0600

    Yet again, I can not get this stupid algo to work

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -29,9 +29,7 @@ def bubble_sort(arr):
     for i in range(0, len(arr) - 1):
         for j in range(i + 1, len(arr)-1):
             if arr[j] > arr[j+1]:
-                temp = arr[j]
-                arr[j] = arr[j + 1]
-                arr[j + 1] = temp
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 '''
