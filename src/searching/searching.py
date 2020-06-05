14471c0505900f8a3861188e98abc6f1c7e044a9
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-06-04 19:32:00 -0600

    Completed the code for linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -4,9 +4,11 @@ def linear_search(arr, target):
     for(start to end of array)
         if (current_element equals to 5)
                 print (current_index);
-
     """
-
+    # Create a for loop that goes through each index that has a value
+    for i in range(0, len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
