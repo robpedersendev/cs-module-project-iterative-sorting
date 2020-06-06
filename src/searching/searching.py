2e4ee5c6e7f8044c854f5d89a2c058ff6ae0468f
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-06-05 21:14:39 -0600

    When running the code it just sits and does nothing. Am I meant to even be a dev?

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,13 +9,29 @@ def linear_search(arr, target):
     for i in range(0, len(arr)):
         if arr[i] == target:
             return i
-    return -1   # not found
+    return -1  # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    if len(arr) == 0:
+        return -1  # array empty
 
-    # Your code here
+    low = 0
+    high = len(arr) - 1
 
+    while low <= high:
+        # go to the middle
+        middle = low + (high - low)  # 2
+        mid = arr[middle]
 
+    # ask if the middle is less than or greater than our target
+    # if less, eliminate the right-hand side
+    if target == mid:
+        return middle
+    elif target > mid:
+        low = middle + 1
+    else:
+        high = middle - 1
+    # adjust the low or high accordingly
     return -1  # not found
